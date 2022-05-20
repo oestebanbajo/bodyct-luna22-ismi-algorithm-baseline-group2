84cf2ac53004e670dcfe4c9bf5f58c3b9423fe80
oestebanbajo <99753914+oestebanbajo@users.noreply.github.com>
2022-05-20 10:13:26 +0200

    Update process.py

--- process.py
+++ process.py
@@ -137,7 +137,7 @@ class Nodule_classifier:
         # Extract the axial/coronal/sagittal center slices of the 50 mm^3 cube
         nodule_data = get_cross_slices_from_cube(volume=nodule_data)
         nodule_data = clip_and_scale(nodule_data)
-        nodule_data = random_flip_augmentation(nodule_data)
+        #nodule_data = random_flip_augmentation(nodule_data)
 
         malignancy = self.model_malignancy(nodule_data[None]).numpy()[0, 1]
         texture = np.argmax(self.model_nodule_type(nodule_data[None]).numpy())

1ed93df8463278cb37314563344108780ca3259b
oestebanbajo <99753914+oestebanbajo@users.noreply.github.com>
2022-06-13 17:25:10 +0200

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -4,6 +4,7 @@ import SimpleITK
 import numpy as np
 from pathlib import Path
 import json
+from typing import List
 
 import statistics
 from statistics import mode
@@ -30,14 +31,108 @@ def clip_and_scale(
     data[data < 0] = 0.0
     return data
 
+def get_model_3d(width, height, depth, num_classes):
+    """
+    Build a 3D convolutional neural network model.
+    """
+    init = tf.keras.initializers.HeNormal()
+
+    inputs_ = tf.keras.Input((width, height, depth))
+    x = layers.Reshape(target_shape=(1, width, height, depth))(inputs_)
+
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv3D(filters=32, kernel_size=3, activation="relu", kernel_initializer=init)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.Conv3D(filters=32, kernel_size=3, activation="relu", kernel_initializer=init)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+
+    x = layers.Dropout(0.3)(x)
+
+    x = layers.Conv3D(filters=64, kernel_size=3, activation="relu", kernel_initializer=init)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+
+    x = layers.Dropout(0.3)(x)
+
+    x = layers.Flatten()(x)
+
+    x = layers.Dense(units=512, activation="relu", kernel_initializer=init)(x)
+    x = layers.Dropout(0.3)(x)
+    x = layers.Dense(units=512, activation="relu", kernel_initializer=init)(x)
+    x = layers.Dropout(0.3)(x)
+
+    outputs = layers.Dense(units=num_classes, activation="softmax", kernel_initializer=init)(x)
+
+    # Define the model.
+    model_ = tf.keras.Model(inputs_, outputs, name="3dcnn")
+    return model_
+
+def get_model_2d(width, height, depth, num_classes):
+    """
+    Build a 2D convolutional neural network model.
+    """
+    init = tf.keras.initializers.HeNormal()
+
+    inputs_ = tf.keras.Input((depth, width, height))
+    #x = layers.Reshape(target_shape=(width, height, depth))(inputs_)
+
+    x = layers.BatchNormalization()(inputs_)
+
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu", kernel_initializer=init)(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.MaxPool2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu", kernel_initializer=init)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPool2D(pool_size=2)(x)
+
+    x = layers.Dropout(0.3)(x)
+
+    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu", kernel_initializer=init)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPool2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu", kernel_initializer=init)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPool2D(pool_size=2)(x)
+
+    x = layers.Dropout(0.3)(x)
+
+    x = layers.Conv2D(filters=128, kernel_size=3, activation="relu", kernel_initializer=init)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPool2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=128, kernel_size=3, activation="relu", kernel_initializer=init)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPool2D(pool_size=2)(x)
+
+    x = layers.Dropout(0.3)(x)
+
+    x = layers.Flatten()(x)
+
+    x = layers.Dense(units=512, activation="relu", kernel_initializer=init)(x)
+    x = layers.Dropout(0.3)(x)
+    x = layers.Dense(units=512, activation="relu", kernel_initializer=init)(x)
+    x = layers.Dropout(0.3)(x)
+
+    outputs = layers.Dense(units=num_classes, activation="softmax", kernel_initializer=init)(x)
+
+    # Define the model.
+    model_ = tf.keras.Model(inputs_, outputs, name="2dcnn")
+    return model_
+
 class Nodule_classifier:
     def __init__(self):
 
-        self.input_size = 224
-        self.input_spacing = 0.2
+        self.input_size_2d = 224
+        self.input_spacing_2d = 0.2
+        
+        self.input_size_3d = 64
+        self.input_spacing_2d = 0.6
 
-        # load malignancy model
-        self.model_malignancy = VGG16(
+        # load malignancy models
+        self.model_malignancy_vgg16 = VGG16(
             include_top=True,
             weights=None,
             input_tensor=None,
@@ -46,44 +141,29 @@ class Nodule_classifier:
             classes=2,
             classifier_activation="softmax",
         )
-        self.model_malignancy.load_weights(
+        self.model_malignancy_vgg16.load_weights(
             "/opt/algorithm/models/vgg16_malignancy_best_val_accuracy.h5",
             by_name=True,
             skip_mismatch=True,
         )
         
-        self.model_densenet_malignancy = tf.keras.applications.densenet.DenseNet201(
-            include_top=True,
-            weights='imagenet',
-            input_tensor=None,
-            input_shape=None,
-            pooling=None,
-            classes=num_classes, classifier_activation='softmax',
-        )
-        self.model_densenet_malignancy.load_weights(
-            "/opt/algorithm/models/densenet_malignancy_best_val_accuracy.h5",
+        self.model_malignancy_2d = get_model_2d(width=224, height=224, depth=3, num_classes=2)
+        self.model_malignancy_2d.load_weights(
+            "/opt/algorithm/models/2dcnn_norotation_malignancy_best_val_accuracy.h5",
             by_name=True,
             skip_mismatch=True,
         )
         
-        self.model_resnet_malignancy = tf.keras.applications.resnet50.ResNet50(
-            include_top=True,
-            weights=None,
-            input_tensor=None,
-            input_shape=None,
-            pooling=None,
-            classes=num_classes,
-            classifier_activation="softmax"),
-        )
-        self.model_resnet_malignancy.load_weights(
-            "/opt/algorithm/models/resnet_malignancy_best_val_accuracy.h5",
+        self.model_malignancy_3d = get_model_3d(width=64, height=64, depth=64, num_classes=2)
+        self.model_malignancy_3d.load_weights(
+            "/opt/algorithm/models/3dcnn_norotation_malignancy_best_val_accuracy.h5",
             by_name=True,
             skip_mismatch=True,
         )
 
 
-        # load texture model
-        self.model_nodule_type = VGG16(
+        # load texture models
+        self.model_nodule_type_vgg16 = VGG16(
             include_top=True,
             weights=None,
             input_tensor=None,
@@ -92,37 +172,22 @@ class Nodule_classifier:
             classes=3,
             classifier_activation="softmax",
         )
-        self.model_nodule_type.load_weights(
+        self.model_nodule_type_vgg16.load_weights(
             "/opt/algorithm/models/vgg16_noduletype_best_val_accuracy.h5",
             by_name=True,
             skip_mismatch=True,
         )
         
-        self.model_resnet_nodule_type = tf.keras.applications.resnet50.ResNet50(
-            include_top=True,
-            weights=None,
-            input_tensor=None,
-            input_shape=None,
-            pooling=None,
-            classes=num_classes,
-            classifier_activation="softmax",
-        )
-        self.model_resnet_nodule_type.load_weights(
-            "/opt/algorithm/models/resnet_noduletype_best_val_accuracy.h5",
+        self.model_nodule_type_2d = get_model_2d(width=224, height=224, depth=3, num_classes=3)
+        self.model_nodule_type_2d.load_weights(
+            "/opt/algorithm/models/2dcnn_noduletype_best_val_accuracy.h5",
             by_name=True,
             skip_mismatch=True,
         )
         
-        self.model_densenet_nodule_type = tf.keras.applications.densenet.DenseNet201(
-            include_top=True,
-            weights='imagenet',
-            input_tensor=None,
-            input_shape=None,
-            pooling=None,
-            classes=num_classes, classifier_activation='softmax',
-        )
-        self.model_densenet_nodule_type.load_weights(
-            "/opt/algorithm/models/densenet_noduletype_best_val_accuracy.h5",
+        self.model_nodule_type_3d = get_model_3d(width=64, height=64, depth=64, num_classes=3)
+        self.model_nodule_type_3d.load_weights(
+            "/opt/algorithm/models/3dcnn_noduletype_best_val_accuracy.h5",
             by_name=True,
             skip_mismatch=True,
         )
@@ -139,49 +204,85 @@ class Nodule_classifier:
     def preprocess(
         self,
         img: SimpleITK.Image,
-    ) -> SimpleITK.Image:
+    ) -> List[SimpleITK.Image]:
 
         # Resample image
         original_spacing_mm = img.GetSpacing()
         original_size = img.GetSize()
-        new_spacing = (self.input_spacing, self.input_spacing, self.input_spacing)
-        new_size = [
+        
+        new_spacing_2d = (self.input_spacing_2d, self.input_spacing_2d, self.input_spacing_2d)
+        new_size_2d = [
             int(round(osz * ospc / nspc))
             for osz, ospc, nspc in zip(
                 original_size,
                 original_spacing_mm,
-                new_spacing,
+                new_spacing_2d,
             )
         ]
-        resampled_img = SimpleITK.Resample(
+        resampled_img_2d = SimpleITK.Resample(
             img,
-            new_size,
+            new_size_2d,
             SimpleITK.Transform(),
             SimpleITK.sitkLinear,
             img.GetOrigin(),
-            new_spacing,
+            new_spacing_2d,
+            img.GetDirection(),
+            0,
+            img.GetPixelID(),
+        )
+        
+        new_spacing_3d = (self.input_spacing_3d, self.input_spacing_3d, self.input_spacing_3d)
+        new_size_3d = [
+            int(round(osz * ospc / nspc))
+            for osz, ospc, nspc in zip(
+                original_size,
+                original_spacing_mm,
+                new_spacing_3d,
+            )
+        ]
+        resampled_img_3d = SimpleITK.Resample(
+            img,
+            new_size_3d,
+            SimpleITK.Transform(),
+            SimpleITK.sitkLinear,
+            img.GetOrigin(),
+            new_spacing_3d,
             img.GetDirection(),
             0,
             img.GetPixelID(),
         )
 
         # Return image data as a numpy array
-        return SimpleITK.GetArrayFromImage(resampled_img)
+        return [SimpleITK.GetArrayFromImage(resampled_img_2d), SimpleITK.GetArrayFromImage(resampled_img_3d)]
 
     def predict(self, input_image: SimpleITK.Image) -> Dict:
 
         print(f"Processing image of size: {input_image.GetSize()}")
 
-        nodule_data = self.preprocess(input_image)
+        nodule_data_2d = self.preprocess(input_image)[0]
+        nodule_data_3d = self.preprocess(input_image)[1]
 
         # Crop a volume of 50 mm^3 around the nodule
-        nodule_data = center_crop_volume(
-            volume=nodule_data,
+        nodule_data_2d = center_crop_volume(
+            volume=nodule_data_2d,
             crop_size=np.array(
                 (
-                    self.input_size,
-                    self.input_size,
-                    self.input_size,
+                    self.input_size_2d,
+                    self.input_size_2d,
+                    self.input_size_2d,
+                )
+            ),
+            pad_if_too_small=True,
+            pad_value=-1024,
+        )
+        
+        nodule_data_3d = center_crop_volume(
+            volume=nodule_data_3d,
+            crop_size=np.array(
+                (
+                    self.input_size_3d,
+                    self.input_size_3d,
+                    self.input_size_3d,
                 )
             ),
             pad_if_too_small=True,
@@ -189,19 +290,21 @@ class Nodule_classifier:
         )
 
         # Extract the axial/coronal/sagittal center slices of the 50 mm^3 cube
-        nodule_data = get_cross_slices_from_cube(volume=nodule_data)
-        nodule_data = clip_and_scale(nodule_data)
-
-        malignancy = self.model_malignancy(nodule_data[None]).numpy()[0, 1]
-        texture = np.argmax(self.model_nodule_type(nodule_data[None]).numpy())
-        malignancy_densenet = self.model_densenet_malignancy(nodule_data[None]).numpy()[0, 1]
-        texture_densenet = np.argmax(self.model_densenet_nodule_type(nodule_data[None]).numpy())
-        malignancy_resnet = self.model_resnet_malignancy(nodule_data[None]).numpy()[0, 1]
-        texture_resnet = np.argmax(self.model_resnet_nodule_type(nodule_data[None]).numpy())
+        nodule_data_2d = get_cross_slices_from_cube(volume=nodule_data_2d)
+        nodule_data_2d = clip_and_scale(nodule_data_2d)
         
-        malignancy_vector = [malignancy,malignancy_densenet,malignancy_resnet]
-        texture_vector = [texture,texture_densenet,texture_resnet]
-        malignancy = np.mean(malignancy_vector)
+        nodule_data_3d = clip_and_scale(nodule_data_3d)
+
+        malignancy_vgg16 = self.model_malignancy_vgg16(nodule_data_2d[None]).numpy()[0, 1]
+        texture_vgg16 = np.argmax(self.model_nodule_type_vgg16(nodule_data_2d[None]).numpy())
+        malignancy_2d = self.model_malignancy_2d(nodule_data_2d[None]).numpy()[0, 1]
+        texture_2d = np.argmax(self.model_nodule_type_2d(nodule_data_2d[None]).numpy())
+        malignancy_3d = self.model_malignancy_3d(nodule_data_3d[None]).numpy()[0, 1]
+        texture_3d = np.argmax(self.model_nodule_type_3d(nodule_data_3d[None]).numpy())
+        
+        malignancy_vector = [malignancy_vgg16,malignancy_2d,malignancy_3d]
+        texture_vector = [texture_vgg16,texture_2d,texture_3d]
+        malignancy = mode(malignancy_vector)
         texture = mode(texture_vector)
 
         result = dict(
